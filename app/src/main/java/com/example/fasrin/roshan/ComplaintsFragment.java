49cb89b0f8fc9894d9ae9f468868bed244cf4faa
Fasrin <fasrinaleem@gmail.com>
2019-09-09 02:09:47 +0530

    Complaint Placed and My Complaints Parts Added, App icon Changed

--- app/src/main/java/com/example/fasrin/roshan/ComplaintsFragment.java
+++ app/src/main/java/com/example/fasrin/roshan/ComplaintsFragment.java
@@ -7,14 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ComplaintsFragment extends Fragment {
-    Button button4;
+    Button button4,button3;
 
     public ComplaintsFragment() {
         // Required empty public constructor
@@ -26,8 +25,10 @@ public class ComplaintsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_complaints, container, false);
-        button4 = v.findViewById(R.id.button4);
+        button4 = v.findViewById(R.id.buttonsend);
+        button3 = v.findViewById(R.id.button3);
         changeComplaints();
+        myComplaints();
         return v;
     }
     public void changeComplaints() {
@@ -36,6 +37,17 @@ public class ComplaintsFragment extends Fragment {
                     @Override
                     public void onClick(View v) {
                         HomeActivity.fm.beginTransaction().replace(R.id.fragment_container, new PlaceAComplaintFragment(), null).commit();
+                    }
+                }
+        );
+    }
+
+    public void myComplaints(){
+        button3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        HomeActivity.fm.beginTransaction().replace(R.id.fragment_container, new ViewComplaintsFragment(), null).commit();
                     }
                 }
         );
--- app/src/main/java/com/example/fasrin/roshan/PlaceAComplaintFragment.java
+++ app/src/main/java/com/example/fasrin/roshan/PlaceAComplaintFragment.java
@@ -6,13 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PlaceAComplaintFragment extends Fragment {
-
+    Button buttonsend;
 
     public PlaceAComplaintFragment() {
         // Required empty public constructor
@@ -23,7 +24,19 @@ public class PlaceAComplaintFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_place_acomplaint, container, false);
+        View v = inflater.inflate(R.layout.fragment_place_acomplaint, container, false);
+        buttonsend = v.findViewById(R.id.buttonsend);
+        myComplaints();
+        return v;
     }
-
+    public void myComplaints(){
+        buttonsend.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        HomeActivity.fm.beginTransaction().replace(R.id.fragment_container, new ComplaintPlacedSuccessFragment(), null).commit();
+                    }
+                }
+        );
+    }
 }
